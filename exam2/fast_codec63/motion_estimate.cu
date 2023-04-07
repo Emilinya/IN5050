c2091038e6bb2c423af3c933de6d7484671244c1
Emilinya <emilid2103@gmail.com>
2023-04-07 13:36:56 +0200

    improved me_block_8x8 reduction

--- exam2/fast_codec63/motion_estimate.cu
+++ exam2/fast_codec63/motion_estimate.cu
@@ -29,6 +29,9 @@ __global__ void me_block_8x8(
 
     int bounds_w = right - left;
     int bounds_h = bottom - top;
+    int size = bounds_w * bounds_h;
+
+    int tid = threadIdx.x + threadIdx.y * bounds_w;
 
     int mx = mb_x * 8;
     int my = mb_y * 8;
@@ -48,27 +51,47 @@ __global__ void me_block_8x8(
                 abssum += abs(origin[u + v*w] - reference[u + v*w]);
             }
         }
-        sad_grid[threadIdx.x + threadIdx.y * bounds_w] = abssum;
+        sad_grid[tid] = abssum;
     }
     
     __syncthreads();
 
-    if (threadIdx.x == 0 && threadIdx.y == 0) {
-        int idx = 2 * (blockIdx.x + blockIdx.y * gridDim.x);
-        int best_sad = INT_MAX;
-        for (int y = 0; y < bounds_h; ++y)
+    // optimal reduction number is sqrt(size) - set reduction number
+    // to first order taylor expansion of sqrt(x) around 640
+    const int reduction_num = 0.01976423537605 * (float)size + 12.64911064067;
+    __shared__ uint16_t best_i_list[32];
+
+    if (tid < reduction_num) {
+        int start = tid * size / reduction_num;
+        int end = (tid + 1) * size / reduction_num;
+
+        int best_i = start;
+        for (int i = start + 1; i < end; ++i)
         {
-            for (int x = 0; x < bounds_w; ++x)
+            if (sad_grid[i] < sad_grid[best_i])
             {
-                int i = x + y * bounds_w;
-                if (sad_grid[i] < best_sad)
-                {
-                    mv_vecs[idx] = left + x - mx;
-                    mv_vecs[idx + 1] = top + y - my;
-                    best_sad = sad_grid[i];
-                }
+                best_i = i;
             }
         }
+        best_i_list[tid] = best_i;
+    }
+
+    __syncthreads();
+
+    if (tid == 0) {
+        int idx = 2 * (blockIdx.x + blockIdx.y * gridDim.x);
+
+        int best_i = best_i_list[0];
+        for (int i = 1; i < reduction_num; ++i)
+        {
+            if (sad_grid[best_i_list[i]] < sad_grid[best_i])
+            {
+                best_i = best_i_list[i];
+            }
+        }
+
+        mv_vecs[idx] = left + (best_i % bounds_w) - mx;
+        mv_vecs[idx + 1] = top + (best_i / bounds_w) - my;
     }
 }
 
